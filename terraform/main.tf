fcd87d73b0c8905a1c7dc78649acd20bdcf6f2e6
nayuta-ai <nayuta723@gmail.com>
2023-01-31 05:09:27 +0900

    Modify dev/deployment.yaml

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,15 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-resource "kubernetes_namespace" "new_namespace" {
+resource "kubernetes_namespace" "argocd_namespace" {
     metadata {
         name = "argocd"
+    }
+}
+
+resource "kubernetes_namespace" "monitoring_namespace" {
+    metadata {
+        name = "monitoring"
     }
 }
 
@@ -19,4 +25,23 @@ resource "helm_release" "my-argocd" {
     repository = "https://charts.bitnami.com/bitnami"
     chart      = "argo-cd"
     namespace  = "argocd"
+
+    # set {
+    #   name = "server.service.type"
+    #   value = "LoadBalancer"
+    # }
+}
+
+resource "helm_release" "my-prometheus" {
+    chart      = "prometheus"
+    name       = "prometheus"
+    namespace  = "monitoring"
+    repository = "https://prometheus-community.github.io/helm-charts"
+}
+
+resource "helm_release" "my-grafana" {
+    chart      = "grafana"
+    name       = "grafana"
+    repository = "https://grafana.github.io/helm-charts"
+    namespace  = "monitoring"
 }
